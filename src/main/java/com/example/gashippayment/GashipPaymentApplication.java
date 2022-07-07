90d0c0d8ab584e7f7c043083830f1799dde7f77d
HoFe-U <hochul-y@naver.com>
2022-07-07 09:41:48 +0900

    first 👑 : gaship-payment 프로젝트 생성

---
+++ src/main/java/com/example/gashippayment/GashipPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.gashippayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GashipPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gashippayment/GashipPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gashippayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GashipPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
